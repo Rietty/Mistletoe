fad564b8b748820a55216866e441a974016ade3c
Juliette H. Lavoie <35241556+Rietty@users.noreply.github.com>
2023-12-11 13:18:21 -0500

    day 11 speedup performance some more

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -1,6 +1,5 @@
 // https://adventofcode.com/2023/day/11
 use std::collections::HashSet;
-use rayon::prelude::*;
 use itertools::Itertools;
 
 const P1_SCALE: i64 = 2;
@@ -27,29 +26,30 @@ pub fn distance(p1: Coordinates, p2: Coordinates) -> i64 {
 }
 
 // We need to be able to calculate the real coordinates based off the scaling of rows and columns without any galaxies in them.
-pub fn get_real_coords(vec: &Vec<Coordinates>, p: &Coordinates, scale: i64) -> Coordinates {
+pub fn get_real_coords(p: &Coordinates, scale: i64, x_set: &HashSet<i64>, y_set: &HashSet<i64>) -> Coordinates {
     // Calculate the offset, that is the amount of numbers by which to shift our coordinate due to any empty row or columns.
-    let x_set: HashSet<_> = vec.iter().map(|key| key.x).collect();
-    let y_set: HashSet<_> = vec.iter().map(|key| key.y).collect();
- 
     let x_offset = (1..=p.x).filter(|&i| !x_set.contains(&i)).count() as i64;
     let y_offset = (1..=p.y).filter(|&i| !y_set.contains(&i)).count() as i64;
 
     // We return the new coordinate, but we have to multiply the offset by scale, and then subtract the offset for the original row or column from the scaled up version.
     Coordinates::new(p.x + (x_offset * scale) - x_offset, p.y + (y_offset * scale) - y_offset)
 }
+ 
+pub fn solve(data: &[Coordinates]) -> (i64, i64) {
+    // Generate hashsets once of the x and y coordinate directions which are needed to get actual coordinates.
+    let x_set: HashSet<_> = data.iter().map(|key| key.x).collect();
+    let y_set: HashSet<_> = data.iter().map(|key| key.y).collect();
 
-pub fn solve(data: &Vec<Coordinates>) -> (i64, i64) {
     // Calculate scaled coordinates before hand for everything.
-    let p1_data: Vec<Coordinates> = data.clone().into_iter().map(|c| get_real_coords(data, &c, P1_SCALE)).collect();
-    let p2_data: Vec<Coordinates> = data.clone().into_iter().map(|c| get_real_coords(data, &c, P2_SCALE)).collect();
+    let p1_data: Vec<Coordinates> = data.iter().map(|c| get_real_coords(&c, P1_SCALE, &x_set, &y_set)).collect();
+    let p2_data: Vec<Coordinates> = data.iter().map(|c| get_real_coords(&c, P2_SCALE, &x_set, &y_set)).collect();
 
-    let p1: i64 = p1_data.iter().combinations(2).par_bridge().map(|c| {
+    let p1: i64 = p1_data.iter().combinations(2).map(|c| {
             distance(*c[0], *c[1])
         }
     ).sum();
 
-    let p2: i64 = p2_data.iter().combinations(2).par_bridge().map(|c| {
+    let p2: i64 = p2_data.iter().combinations(2).map(|c| {
             distance(*c[0], *c[1])
         }
     ).sum();
@@ -79,7 +79,13 @@ pub fn run() {
 #[allow(dead_code)]
 pub fn benchmark(c: &mut criterion::Criterion) {
     let data = parse(&crate::library::read_file("data/day11.txt"));
-    c.bench_function("Day 11", |b| b.iter(|| solve(&data)));
+    c.bench_function("Day 11 - solve:", |b| b.iter(|| solve(&data)));
+    c.bench_function("Day 11 - parse & solve:", |b| {
+        b.iter(|| {
+            let data = parse(&crate::library::read_file("data/day11.txt"));
+            solve(&data)
+        })
+    });
 }
 
 #[cfg(test)]
