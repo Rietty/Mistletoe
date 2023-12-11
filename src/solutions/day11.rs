0cecd86a1a1e29228d58e155909edbf92188bc6c
Juliette Lavoie <rie.juliette@gmail.com>
2023-12-11 18:24:39 -0500

    fix day 11 formatting

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -1,6 +1,6 @@
 // https://adventofcode.com/2023/day/11
-use std::collections::HashSet;
 use itertools::Itertools;
+use std::collections::HashSet;
 
 const P1_SCALE: i64 = 2;
 const P2_SCALE: i64 = 1_000_000;
@@ -8,15 +8,12 @@ const P2_SCALE: i64 = 1_000_000;
 #[derive(Hash, Eq, PartialEq, Debug, Copy, Clone)]
 pub struct Coordinates {
     x: i64,
-    y: i64
+    y: i64,
 }
 
 impl Coordinates {
     fn new(x: i64, y: i64) -> Coordinates {
-        Coordinates {
-            x,
-            y
-        }
+        Coordinates { x, y }
     }
 }
 
@@ -26,33 +23,50 @@ pub fn distance(p1: Coordinates, p2: Coordinates) -> i64 {
 }
 
 // We need to be able to calculate the real coordinates based off the scaling of rows and columns without any galaxies in them.
-pub fn get_real_coords(p: &Coordinates, scale: i64, x_set: &HashSet<i64>, y_set: &HashSet<i64>) -> Coordinates {
+pub fn get_real_coords(
+    p: &Coordinates,
+    scale: i64,
+    x_set: &HashSet<i64>,
+    y_set: &HashSet<i64>,
+) -> Coordinates {
     // Calculate the offset, that is the amount of numbers by which to shift our coordinate due to any empty row or columns.
     let x_offset = (1..=p.x).filter(|&i| !x_set.contains(&i)).count() as i64;
     let y_offset = (1..=p.y).filter(|&i| !y_set.contains(&i)).count() as i64;
 
     // We return the new coordinate, but we have to multiply the offset by scale, and then subtract the offset for the original row or column from the scaled up version.
-    Coordinates::new(p.x + (x_offset * scale) - x_offset, p.y + (y_offset * scale) - y_offset)
+    Coordinates::new(
+        p.x + (x_offset * scale) - x_offset,
+        p.y + (y_offset * scale) - y_offset,
+    )
 }
- 
+
 pub fn solve(data: &[Coordinates]) -> (i64, i64) {
     // Generate hashsets once of the x and y coordinate directions which are needed to get actual coordinates.
     let x_set: HashSet<_> = data.iter().map(|key| key.x).collect();
     let y_set: HashSet<_> = data.iter().map(|key| key.y).collect();
 
     // Calculate scaled coordinates before hand for everything.
-    let p1_data: Vec<Coordinates> = data.iter().map(|c| get_real_coords(&c, P1_SCALE, &x_set, &y_set)).collect();
-    let p2_data: Vec<Coordinates> = data.iter().map(|c| get_real_coords(&c, P2_SCALE, &x_set, &y_set)).collect();
+    let p1_data: Vec<Coordinates> = data
+        .iter()
+        .map(|c| get_real_coords(&c, P1_SCALE, &x_set, &y_set))
+        .collect();
+    
+    let p2_data: Vec<Coordinates> = data
+        .iter()
+        .map(|c| get_real_coords(&c, P2_SCALE, &x_set, &y_set))
+        .collect();
 
-    let p1: i64 = p1_data.iter().combinations(2).map(|c| {
-            distance(*c[0], *c[1])
-        }
-    ).sum();
+    let p1: i64 = p1_data
+        .iter()
+        .combinations(2)
+        .map(|c| distance(*c[0], *c[1]))
+        .sum();
 
-    let p2: i64 = p2_data.iter().combinations(2).map(|c| {
-            distance(*c[0], *c[1])
-        }
-    ).sum();
+    let p2: i64 = p2_data
+        .iter()
+        .combinations(2)
+        .map(|c| distance(*c[0], *c[1]))
+        .sum();
 
     (p1, p2)
 }
