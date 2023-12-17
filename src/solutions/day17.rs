e5f3637206840f621c4e076c4ec1e3ccd787792a
Juliette Lavoie <rie.juliette@gmail.com>
2023-12-17 09:48:53 -0500

    finish day 17

--- src/solutions/day17.rs
+++ src/solutions/day17.rs
@@ -5,19 +5,11 @@ use crate::library::{
 };
 use std::collections::{binary_heap::BinaryHeap, HashMap, HashSet};
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub enum Direction {
-    Up,
-    Right,
-    Down,
-    Left,
-}
-
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub struct Spot {
-    pub pos: Point,
-    pub dir: Direction, // The dir is actually the direction we came from, and thus can't go back.
-    pub cost: i32,
+    pos: Point,
+    dir: i32, // The dir is actually the direction we came from, and thus can't go back.
+    cost: i32,
 }
 
 // Implement Ord and PartialOrd for Spot, so we can use it in a BinaryHeap, we need to sort by cost.
@@ -34,29 +26,30 @@ impl PartialOrd for Spot {
 }
 
 // Function that will return a Position that contains a delta on which way to move based off direction.
-pub fn get_delta(dir: Direction) -> Point {
+pub fn get_delta(dir: i32) -> Point {
     match dir {
-        Direction::Up => Point { x: 0, y: -1 },
-        Direction::Right => Point { x: 1, y: 0 },
-        Direction::Down => Point { x: 0, y: 1 },
-        Direction::Left => Point { x: -1, y: 0 },
+        0 => Point { x: 0, y: 1 },
+        1 => Point { x: 1, y: 0 },
+        2 => Point { x: 0, y: -1 },
+        3 => Point { x: -1, y: 0 },
+        _ => unreachable!(),
     }
 }
 
 // This function will try to flow the lava according to the constraints about distance traveled.
-pub fn flow(data: &Grid, min: usize, max: usize) -> i32 {
+pub fn flow(data: &Grid, min: usize, max: usize, sdir: i32) -> i32 {
     // Create a queue of spots to flow from, always starts from top-left.
     let mut queue = BinaryHeap::new();
 
     queue.push(Spot {
         pos: Point { x: 0, y: 0 },
-        dir: Direction::Down,
+        dir: sdir,
         cost: 0,
     });
 
     // Create a HashSet of visited/seen spots.
-    let mut visited: HashSet<(Point, Direction)> = HashSet::new();
-    let mut costs: HashMap<(Point, Direction), i32> = HashMap::new();
+    let mut visited: HashSet<(Point, i32)> = HashSet::new();
+    let mut costs: HashMap<(Point, i32), i32> = HashMap::new();
 
     // While the queue is not empty, keep flowing.
     while !queue.is_empty() {
@@ -77,24 +70,19 @@ pub fn flow(data: &Grid, min: usize, max: usize) -> i32 {
         visited.insert((spot.pos, spot.dir));
 
         // For each of the four directions, we will try to flow in that direction.
-        for dir in [
-            Direction::Up,
-            Direction::Right,
-            Direction::Down,
-            Direction::Left,
-        ]
-        .iter()
-        {
+        for dir in 0..4 {
+            // Amount of increase for the new cost.
             let mut increase = 0;
+
             // If the direction is the same as the direction we came from, we can't go back.
-            if *dir == spot.dir {
+            if dir == spot.dir || dir == (spot.dir + 2) % 4 {
                 continue;
             }
 
             // For the range of min to max, we will try to flow in that direction.
             for dist in 1..max + 1 {
                 // Calculate the new amount of increase for X and Y for the current direction.
-                let delta = get_delta(*dir);
+                let delta = get_delta(dir);
                 let new_x = spot.pos.x + delta.x * dist as i32;
                 let new_y = spot.pos.y + delta.y * dist as i32;
 
@@ -112,22 +100,27 @@ pub fn flow(data: &Grid, min: usize, max: usize) -> i32 {
                         .unwrap()
                         .to_digit(10)
                         .unwrap();
+
                     // If distance is less than min, we can't flow.
                     if dist < min {
                         continue;
                     }
+
                     let nc = spot.cost + increase as i32;
+
                     // Try to read from hashmap, if it doesn't exist, default to 1000000.
                     let c = *costs
-                        .get(&(Point { x: new_x, y: new_y }, *dir))
+                        .get(&(Point { x: new_x, y: new_y }, dir))
                         .unwrap_or(&1000000);
+
                     if c <= nc {
                         continue;
                     }
-                    costs.insert((Point { x: new_x, y: new_y }, *dir), nc);
+
+                    costs.insert((Point { x: new_x, y: new_y }, dir), nc);
                     queue.push(Spot {
                         pos: Point { x: new_x, y: new_y },
-                        dir: *dir,
+                        dir: dir,
                         cost: nc,
                     });
                 }
@@ -139,8 +132,8 @@ pub fn flow(data: &Grid, min: usize, max: usize) -> i32 {
 }
 
 pub fn solve(data: &Grid) -> (i32, i32) {
-    let p1 = flow(data, 1, 3);
-    let p2 = flow(data, 4, 10);
+    let p1 = std::cmp::min(flow(data, 1, 3, 0), flow(data, 1, 3, 1));
+    let p2 = std::cmp::min(flow(data, 4, 10, 0), flow(data, 4, 10, 1));
     (p1, p2)
 }
 
